91986c4e8c1eca00e21a7751ca5424047773dd5d
ZinhNguyen <67100275+ZinhNguyen@users.noreply.github.com>
2022-03-14 17:36:58 +0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ let video;
 
 async function init(){
     const detectorConfig = {
-      modelType: poseDetection.movenet.modelType.SINGLEPOSE_LIGHTNING
+      modelType: poseDetection.movenet.modelType.SINGLEPOSE_LIGHTNING,
     };
     detector = await poseDetection.createDetector(
       poseDetection.SupportedModels.MoveNet, 
@@ -18,8 +18,9 @@ async function videoReady(){
 }
 
 async function setup() {
-  createCanvas(640, 480);
+  createCanvas(640, 480); 
   video = createCapture(VIDEO, videoReady);
+  //video = createCapture(VIDEO);
   //video.size(320, 240);
   video.hide();
   await init();
@@ -28,22 +29,26 @@ async function setup() {
 
 async function getPoses(){ 
   poses = await detector.estimatePoses(video.elt);
-  //console.log(poses);
+  // console.log(poses);
   setTimeout(getPoses, 0);
 }
 
 function draw() {
   background(220);
-  image(video, 0, 0);
+  image(video, 0, 0, 640, 480);
   if (poses && poses.length > 0) {
     for (let kp of poses[0].keypoints){
-  const {x, y, score } = kp;
-  if (score > 0.5){
-  fill(255);
-  stroke(0);
-  strokeWeight(4);
-  circle(x, y, 12);
-  }
-  }
+      const {x, y, score } = kp;
+      if (score > 0.5){
+        console.log(poses[0].keypoints[0].y);
+        fill(255);
+        stroke(0);
+        strokeWeight(4);
+        circle(x, y, 12);
+        fill(0,255,0);
+        textSize(32);
+        text('Fall Detection', 50, 50);
+      }
+    }
   }
 }
