4073ae2302700e05e5b132a4341da178e4df2201
ZinhNguyen <67100275+ZinhNguyen@users.noreply.github.com>
2022-03-16 17:14:41 +0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,12 @@
 let detector;
 let poses;
 let video;
+let y_nose1;
+let y_nose2;
+let y_knee1;
+let y_knee2;
+let a = 1;
+let Fall = 0;
 
 async function init(){
     const detectorConfig = {
@@ -24,30 +30,52 @@ async function setup() {
   //video.size(320, 240);
   video.hide();
   await init();
-  //createButton('pose').mousePressed(getPoses);
+  //createButton('pose').mousePressed(getPoses1);
 }
 
 async function getPoses(){ 
   poses = await detector.estimatePoses(video.elt);
-  // console.log(poses);
-  setTimeout(getPoses, 0);
+  // console.log(poses[0].keypoints[0].y);
+  if(poses[0].keypoints[0].y > y_knee1 || poses[0].keypoints[0].y > y_knee2){
+    a+=1;
+    print(a);
+    Fall = 1;
+  }
+  else Fall = 0;
+  y_nose1 = poses[0].keypoints[0].y;
+  y_knee1 = poses[0].keypoints[13].y;
+  y_knee2 = poses[0].keypoints[14].y;
+  // console.log('y1= ', y_nose1);
+  setTimeout(getPoses, 1000);
+}
+
+async function getPoses1(){ 
+  poses1 = await detector.estimatePoses(video.elt);
+  y_nose2 = poses1[0].keypoints[0].y
+  console.log(y_nose2);
+  setTimeout(getPoses, 2000);
 }
 
 function draw() {
   background(220);
   image(video, 0, 0, 640, 480);
+  //print(y_nose1);
   if (poses && poses.length > 0) {
     for (let kp of poses[0].keypoints){
       const {x, y, score } = kp;
       if (score > 0.5){
-        console.log(poses[0].keypoints[0].y);
-        fill(255);
-        stroke(0);
-        strokeWeight(4);
-        circle(x, y, 12);
-        fill(0,255,0);
-        textSize(32);
-        text('Fall Detection', 50, 50);
+        //console.log(poses[0].keypoints);
+        //y_nose2 = poses[0].keypoints[0].y;
+        // fill(255);
+        // stroke(0);
+        // strokeWeight(4);
+        // circle(x, y, 12);
+        if(Fall == 1) {
+          //console.log(y_nose1);
+          fill(0,255,0);
+          textSize(32);
+          text('Fall Detection', 50, 50);
+        }
       }
     }
   }
