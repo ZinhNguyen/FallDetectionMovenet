1459199a6d0ba3c31fd92bbbfe7a4d3bdbb01c94
ZinhNguyen <67100275+ZinhNguyen@users.noreply.github.com>
2022-04-04 17:56:41 +0700

    Update skeleton

--- sketch.js
+++ sketch.js
@@ -26,17 +26,18 @@ let Count = true;
 let timer= 0;
 let flag_stand = 0;
 let vid = 1;
-let limit_video = 40;
+let limit_video = 30;
 //Fall dataset
-// var fall_dataset = 'video/fall-';
-var fall_dataset = 'video/adl-';
+var fall_dataset = 'video/fall-';
+// var fall_dataset = 'video/adl-';
 var fall_default = '-cam0.mp4';
-//var dataset = 'video/fall-'+ vid +'-cam0.mp4';
+// var dataset = 'video1/fall-13-cam1.mp4';
 //NoFall dataset
 //var dataset = 'video/adl-'+ vid +'-cam0.mp4';
-
+const ScoreThreshold = 0.4;
 
 // Initialize for MoveNet model
+      //SINGLEPOSE_LIGHTNING , SINGLEPOSE_THUNDER, MULTIPOSE_LIGHTNING
 async function init(){
     const detectorConfig = {
       modelType: poseDetection.movenet.modelType.SINGLEPOSE_LIGHTNING,
@@ -62,40 +63,47 @@ async function setup() {
   //createCanvas(640, 480); 
   createCanvas(640, 480); 
   // video = createCapture(VIDEO, videoReady);
-  //video = createVideo('video/fall-'+ vid +'-cam0.mp4');
-  video = createVideo(fall_dataset + vid + fall_default);
+  // video = createVideo(fall_dataset + vid + fall_default);
+  video = createVideo('video1/' + 33 + '.mp4');
+  // video = createVideo(dataset);
   //console.log(vid);
   //video.play();
   //video.loop();
-  //video.size(1280, 480);
+  // video.size(640, 480);
   video.hide();
   await init();
   await getPoses();
-  //createButton('pose').mousePressed(getPoses);
-  // createButton('Pause').mousePressed(pause);
+  // createButton('pose').mousePressed(getPoses1);
+  // createButton('Pause').mousePressed(pauseAndGetPose1);
   // createButton('Play').mousePressed(play);
   //if (ready == 1) {
   video.play();
   //}
 }
 
-function pause(){
+function pauseAndGetPose1(){
   video.pause();
+  getPoses1();
 }
 function play(){
   video.play();
 }
 
+async function getPoses1(){
+  poses = await detector.estimatePoses(video.elt);
+  print(poses);
+}
+
 
 async function getPoses(){ 
   //ready = 1;
-  if (ready == 1){
+  // if (ready == 1){
     poses = await detector.estimatePoses(video.elt);
-  }
+  // }
   //console.log(poses[0].keypoints[0].y);
   // ready = 1;
   if (poses && poses.length > 0){
-    // console.log(poses);
+    // console.log(poses[0]);
     // First condition
     if(poses[0].keypoints[0].y >= y_knee1 || poses[0].keypoints[0].y >= y_knee2){
       if(angle1 >= 45 || angle2 >= 45){
@@ -109,9 +117,10 @@ async function getPoses(){
         } 
       }
     }
-    if(poses[0].keypoints[0].y < flag_stand && weight_body < height_body && angle1 < 45 && angle2 < 45){
-      Fall = 0;
-    }
+    // Stand up after fall condition
+    // if(poses[0].keypoints[0].y < flag_stand && weight_body < height_body && angle1 < 45 && angle2 < 45){
+    //   Fall = 0;
+    // }
     //else Fall = 0;
     // // Second condition
     // if(angle1 > 45 || angle2 > 45){
@@ -146,8 +155,64 @@ async function getPoses(){
     x_ankle2 = poses[0].keypoints[16].x;
     y_ankle2 = poses[0].keypoints[16].y;
   }
-  
   setTimeout(getPoses, timer);
+}
+
+function increaseVideo(){
+  if (vid < limit_video){
+    a = 0;
+    vid++;
+    Count = true;
+    setup();
+  }
+}
+
+const skeleton = [
+	[0, 1],
+	[0, 2],
+	[1, 3],
+	[2, 4],
+	[[6, 5]],
+	[6, 5],
+	[5, 7],
+	[6, 8],
+	[7, 9],
+	[8, 10],
+	[[5, 6],[11, 12]],
+	[[11, 12], 11],
+	[[11, 12], 12],
+	[11, 13],
+	[12, 14],
+	[13, 15],
+	[14, 16],
+];
+
+function getKeypointForEdgeVertex(keypoints, vertex) {
+	if (typeof vertex === "number") {
+		const {
+			x,
+			y,
+			score
+		} = keypoints[vertex];
+		if (score > ScoreThreshold) {
+			return { x, y };
+		}
+	} else if (vertex instanceof Array) {
+		const points = vertex.map(v => keypoints[v]);
+		if (points.every(kp => kp.score > ScoreThreshold)) {
+			const { x, y } =
+						// Average the points
+						points.reduce(
+							(acc, v) => ({
+								x: (acc.x * acc.w + v.x) / (acc.w + 1),
+								y: (acc.y * acc.w + v.y) / (acc.w + 1),
+								w: acc.w + 1
+							}),
+							{ x: 0, y: 0, w: 0 }
+						);
+		  return { x, y };
+		}
+	}
 }
 
 function draw() {
@@ -155,7 +220,6 @@ function draw() {
   image(video, 0, 0);
   // filter(THRESHOLD);
   //print(video.elt);
-  //console.log(poses);
   if (poses && poses.length > 0) {
     for (let kp of poses[0].keypoints){
       const {x, y, score } = kp;
@@ -163,8 +227,8 @@ function draw() {
         //console.log(poses[0].keypoints);
         fill(255);
         stroke(0);
-        strokeWeight(4);
-        circle(x, y, 12);
+        strokeWeight(2);
+        circle(x, y, 6);
         //test angle
         fill(0,255,0);
         textSize(20);
@@ -172,7 +236,17 @@ function draw() {
         text(poses[0].keypoints[0].y, 50, 150);
       }
     }
+    stroke('yellow');
+    strokeWeight(2);
+    for (let edge of skeleton) {
+      let start = getKeypointForEdgeVertex(poses[0].keypoints, edge[0]);
+      let end = getKeypointForEdgeVertex(poses[0].keypoints, edge[1]);
+      if (start && end) {
+        line(start.x, start.y, end.x, end.y);
+      }
+    }
   }
+  stroke(0);
   if(Fall == 1) {
     //console.log(y_nose1);
     fill(255,0,0);
@@ -193,12 +267,7 @@ function draw() {
     } else {
       console.log('Video ', vid, ': No Fall');
     }
-    if (vid < limit_video){
-      a = 0;
-      vid++;
-      Count = true;
-      setup();
-    }
+    increaseVideo();
   }
   if (video.loadedmetadata == true){
     ready = 1;
